e37828cc9914dfbad5ec5cdf8694e5fd140033da
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-20 00:36:40 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name ,language)
-  puts "Hi, my name is #{name} and I am learning to pogram in #{language}"
+  puts "Hi, my name is #{name} and I am learning to pogram in #{language}."
 end
